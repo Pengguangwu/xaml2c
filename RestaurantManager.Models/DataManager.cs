398b42d87af0a128281957d82efae5d9428ba545
Pengguang Wu <pgwu@hotmail.com>
2015-12-23 18:40:11 -0800

    23 ed

---
+++ RestaurantManager.Models/DataManager.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace RestaurantManager.Models
+
+{
+    public abstract class DataManager : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected RestaurantContext Repository { get; private set; }
+ 
+        public DataManager()
+        {
+            LoadData();
+   
+        }
+
+        private async void LoadData()
+        {
+            this.Repository = new RestaurantContext();
+            await this.Repository.InitializeContextAsync();
+            OnDataLoaded();
+        }
+
+        protected abstract void OnDataLoaded();
+
+        public void OnPropertyChanged([CallerMemberName]string propName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+
+    }
+}
---
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RestaurantManager.Models
+{
+    public class ExpediteDataManager : DataManager
+    {
+        protected override void OnDataLoaded()
+        {
+            
+         this.OrderItems = base.Repository.Orders;
+ 
+        }
+
+        public List<Order> OrderItems
+        {
+            get { return base.Repository.Orders; }
+            set { OnPropertyChanged(); }
+        }
+        
+    }
+}
